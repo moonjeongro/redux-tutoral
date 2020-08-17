5a919ce17f24ca60862881335a3a69bfc99fb416
moonjeongro <moonjeongro@gmail.com>
2020-08-17 15:36:38 +0900

    change counter using redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
+import {createStore} from "redux";
+
 const add = document.getElementById("button--add");
 const minus = document.getElementById("button--minus");
 const number = document.getElementById("span--number");
 
-let count = 0;
-number.innerText = count;
+number.innerText = 0;
 
-function updateText(){
-    number.innerText = count;
+// only one function that can modify the 'countStore'
+// action is the way we can communicate with the modifier
+// whatever reducer returned that will be a state of your application
+const countModifier = (count = 0, action) => {
+    if (action.type === "ADD") {
+        return count + 1;
+    } else if (action.type === "MINUS") {
+        return count - 1;
+    } else {
+        return count;
+    }
+};
+
+const countStore = createStore(countModifier);
+
+const onChange = () => {
+    number.innerText = countStore.getState();
 }
 
-function handleAdd(event){
-    count++;
-    updateText();
-}
+// If we want listen changing the state we can subscribe
+countStore.subscribe(onChange);
 
-function handleMinus(event){
-    count--;
-    updateText();
-}
+// dispatch call reducer with an action
+// action must be an object
+// action must have the 'type' element and we cannot modify its name. 
+add.addEventListener("click", () => countStore.dispatch({type: "ADD" }))
+minus.addEventListener("click", () => countStore.dispatch({type: "MINUS" }))
 
-add.addEventListener("click", handleAdd)
-minus.addEventListener("click", handleMinus)
+console.log(countStore.getState())
